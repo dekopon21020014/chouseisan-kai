cb781b991d2f3ce78242cc9381fb6575a9b779b5
dekopon21020014 <renueta@icloud.com>
2024-05-18 11:26:27 +0900

    github.com/nattn/go-sqlite3を導入

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	router := gin.Default()
+	db, err := sql.Open("sqlite3", "database.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	router := gin.Default()		
 	router.Run()
 }
